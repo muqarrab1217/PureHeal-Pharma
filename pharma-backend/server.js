c2e8b39b42c02a8ad6a353dbdbd5cd32d76e944a
Muqarrab Ahmed <muq1217@gmail.com>
2025-06-06 00:03:30 +0500

    Update server.js

--- pharma-backend/server.js
+++ pharma-backend/server.js
@@ -16,8 +16,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
   console.log("✅ MongoDB connected");
 })
 .catch((err) => {
